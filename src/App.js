49393e0a6dd9a3966ea0cc3f30f6e3da3fcb7895
purewack <bobimaster15@gmail.com>
2021-10-22 18:52:04 +0100

    transport integration

--- src/App.js
+++ src/App.js
@@ -3,56 +3,76 @@ import * as Tone from 'tone'
 import './App.css';
 
 function App() {
+  const [begun, setBegun] = useState(false)
+  const [transport, setTransport] = useState('0:0:0')
   const [recording, setRecording] = useState(false)
   const mediaRecorder = useRef(null)
   const chunkz = useRef([])
   const player = useRef(null)
-  const aud = useRef(null)
   
-  useEffect(()=>{
-    navigator.mediaDevices.getUserMedia({audio:true,video:false}).then(stream => {
-      console.log('permission granted')
-      
-      mediaRecorder.current = new MediaRecorder(stream, {mimeType:'audio/webm'});
-      mediaRecorder.current.ondataavailable = (e)=>{
-        if(e.data.size > 0) chunkz.current.push(e.data)
+  useEffect(()=> {
+
+    if(begun){
+      navigator.mediaDevices.getUserMedia({audio:true,video:false}).then(stream => {
+        console.log('permission granted')
+        
+        mediaRecorder.current = new MediaRecorder(stream, {mimeType:'audio/webm'});
+        mediaRecorder.current.ondataavailable = (e)=>{
+          if(e.data.size > 0) chunkz.current.push(e.data)
+          //console.log(e)
+        }
+        mediaRecorder.current.onstart = (e)=>{
+          console.log('started')
+          chunkz.current = []
+        }
+        mediaRecorder.current.onstop = (e)=>{
+          let source = URL.createObjectURL(new Blob(chunkz.current))
+          console.log(source)
+          player.current = new Tone.Player(source).toDestination().sync().start('1:0:0')
+        }
+        mediaRecorder.current.onerror = (e)=>{
+          console.log('error')
+        }
+        console.log(mediaRecorder.current)
+      })
+      const startTone = async ()=>{
+        await Tone.start()
       }
-      mediaRecorder.current.onstart = (e)=>{
-        console.log('started')
-        chunkz.current = []
-      }
-      mediaRecorder.current.onstop = (e)=>{
-        let source = URL.createObjectURL(new Blob(chunkz.current))
-        console.log(source)
-        player.current = new Tone.Player(source).toDestination()
-        player.current.autostart = true
-      }
-      mediaRecorder.current.onerror = (e)=>{
-        console.log('error')
-      }
-      console.log(mediaRecorder.current)
-    })
-  },[])
+      startTone()
+    }
+
+  },[begun])
 
   const recHandle = ()=>{
-    setRecording(true)
-    mediaRecorder.current.start()
-  }
-  const playHandle = ()=>{
-    setRecording(false)
     if(mediaRecorder.current.state === 'recording'){
-    mediaRecorder.current.stop()
+      mediaRecorder.current.stop()
+      setRecording(false)
     }
     else{
-    player.current.start(0)
+      mediaRecorder.current.start(1000*1024/44100)
+      setRecording(true)
     }
   }
+  const playHandle = ()=>{
+    if(Tone.Transport.state === 'started'){
+      Tone.Transport.stop()
+    }
+    else{
+      Tone.Transport.seconds = 0
+      Tone.Transport.start().scheduleRepeat(transport=>{
+        setTransport(Tone.Transport.position)
+      },'16n',0)
+    }
+    setRecording(false)
+  }
 
-  return (<>
-    <p> Simple DAW {recording ? 'recording' : 'ready'}</p>
+  return (<> {!begun ? <button onClick={()=>{setBegun(true)}}>Begin</button> :
+    <>
+    <p> Simple DAW - {recording ? 'recording' : 'ready'}</p>
     <button onClick={recHandle}>Record</button>
     <button onClick={playHandle}>Playback</button>
-    <audio ><source ref={aud} src='' type='audio/mpeg'/>Output</audio>
+    <p>{transport}</p>
+    </>}
   </>);
 }
 
