83cd3f5bd0b23ff503a1fea9e2cb20cdc1914687
Damian Nowacki <bobimaster15@gmail.com>
2021-11-03 23:53:19 +0000

    drag events in track, left resize on snap bug

--- src/components/AudioRegion.js
+++ src/components/AudioRegion.js
@@ -1,24 +1,15 @@
 import './components.css';
 import {useState,useEffect,useRef} from 'react'
 
-const AudioRegion = ({region, setRegion, bar, shouldSnap})=>{
+const AudioRegion = ({region, setRegion, bar, shouldSnap, mousePos})=>{
   const [left, setLeft] = useState()
   const [width, setWidth] = useState()
   const [drag, setDrag] = useState()
   const [hover, setHover] = useState()
   const preClick = useRef()
-  const resizeArea = 15;
+  const mouseAreaRef = useRef()
+  const resizeArea = 10;
 
-  const collisionCalc = (x,r)=>{
-    let d = 'center'
-    if(x > r.left && x < r.left+resizeArea){
-      d = 'left'
-    } 
-    if(x < r.right && x > r.right-resizeArea){
-      d = 'right'
-    }
-    return d
-  }
   const snapCalc = (ll)=>{
     if(shouldSnap){
       let b = (bar/shouldSnap)
@@ -33,29 +24,16 @@ const AudioRegion = ({region, setRegion, bar, shouldSnap})=>{
     setWidth(bar*region.timeDuration)
   },[region,bar])
 
-  const mouseDown = (e)=>{
-    e.preventDefault()
-    let x = e.clientX
-    let r = e.target.getBoundingClientRect()
-    preClick.current = {x ,r, left:snapCalc(left), width}
-    setDrag(collisionCalc(x,r))
-
-  }
-  const mouseMove = (e)=>{
-    e.preventDefault()
-    let x = e.clientX
-    let r = e.target.getBoundingClientRect()
-    setHover(collisionCalc(x,r))
+  useEffect(()=>{
     if(!drag) return
-
     let pe = preClick.current
-    let delta = snapCalc(e.clientX - pe.x )   
+    let delta = snapCalc(mousePos - pe.x )
     switch (drag) {
-      case 'right':
+      case 'EndHandle':
         setWidth(pe.width + delta)
         break;
       
-      case 'left':
+      case 'StartHandle':
         setLeft(pe.left + delta)
         setWidth(pe.width - delta)
         break;
@@ -64,7 +42,12 @@ const AudioRegion = ({region, setRegion, bar, shouldSnap})=>{
         setLeft(pe.left + delta)
         break;
     }
+  },[mousePos])
 
+  const mouseDown = (e)=>{
+    e.preventDefault()
+    preClick.current = {x:e.clientX ,left:snapCalc(left), width}
+    setDrag(e.target.className)
   }
   const mouseUp = (e)=>{
     e.preventDefault()
@@ -72,18 +55,16 @@ const AudioRegion = ({region, setRegion, bar, shouldSnap})=>{
     //setRegion()
   }
 
-const pointerEvents = {pointerEvents:'none', width:resizeArea}
+  const pointerEvents = {width:resizeArea}
 
-  return(<div 
+  return(<div
     className={drag ? 'AudioRegion AudioRegionDrag' : 'AudioRegion'} 
-    style={{width, left, cursor:((hover === 'center' || !hover) ? 'grab' : 'col-resize')}}
+    style={{width, left}}
     onMouseDown={mouseDown}
-    onMouseMove={mouseMove}
     onMouseUp={mouseUp}
-    onMouseLeave={mouseUp}
     >
     <span className='StartHandle' style={pointerEvents}>|</span>
-    <span></span>
+    <span> </span>
     <span className='EndHandle' style={pointerEvents}>|</span>  
   </div>)
 }
--- src/components/AudioTrack.js
+++ src/components/AudioTrack.js
@@ -1,14 +1,31 @@
-import {useState} from 'react'
+import {cloneElement, useState} from 'react'
 import './components.css';
 import AudioRegion from './AudioRegion';
 
 
 const AudioTrack = ({id, armedId, onArm, onSolo, onMute, children})=>{
- 
-    return(<div className={armedId === id ? 'AudioTrack AudioTrackArmed' : 'AudioTrack'}>
-        <div className="content">
-            {children}
-        </div>
+    
+    const [mousePos, setMousePos] = useState()
+    const [drag, setDrag] = useState(false)
+
+    return(<div className={armedId === id ? 'AudioTrack AudioTrackArmed' : 'AudioTrack'}
+    onMouseDown={(e)=>{
+        e.preventDefault()
+        setDrag(true)
+    }}
+    onMouseUp={(e)=>{
+        e.preventDefault()
+        setDrag(false)
+        setMousePos(null); 
+    }}
+    onMouseMove={(e)=>{
+        e.preventDefault()
+        if(drag) setMousePos(e.clientX)
+    }}
+    >
+        {children.map(c => {
+            return cloneElement(c, { mousePos })
+        })}
     </div>)
 }
 
